8e88016ef98137689b43a004957b3b399f56803e
VanLuu123 <vluu3449@sdsu.edu>
2025-05-03 22:30:46 -0700

    adjusted loading ui

--- frontend/app/components/BounceLoading.tsx
+++ frontend/app/components/BounceLoading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 // Bounce loading animation
 const BounceLoading = () => {
   return (
-    <div className="flex gap-1 items-center bg-gray-200 rounded-lg">
+    <div className="flex gap-1 items-center rounded-lg">
       <div className="w-2 h-2 bg-gray-700 rounded-full animate-bounce [animation-delay:-0.3s]" />
       <div className="w-2 h-2 bg-gray-700 rounded-full animate-bounce [animation-delay:-0.15s]" />
       <div className="w-2 h-2 bg-gray-700 rounded-full animate-bounce" />
