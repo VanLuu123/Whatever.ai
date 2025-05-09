f202c03400a82c3693acff9626ef99672b309740
VanLuu123 <53393660+VanLuu123@users.noreply.github.com>
2025-05-08 17:37:06 -0700

    fix errors

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -45,15 +45,13 @@ export default function RootLayout({
         <ClerkProvider>
           <ChatProvider>
             <Navbar />
-            <header className="flex justify-end items-center p-4 gap-4 h-16">
-              <SignedOut>
-                <SignInButton />
-                <SignUpButton />
-              </SignedOut>
-              <SignedIn>
-                <UserButton />
-              </SignedIn>
-            </header>
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
             {children}
           </ChatProvider>
         </ClerkProvider>
